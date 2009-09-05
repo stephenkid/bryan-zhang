75c407f3904dba4c40d59434a1b5dfa2abac0702
bryan.zhang.31 <bryan.zhang.31@c3c2f732-976e-11de-9e98-d1f6c90d23ca>
2009-09-05 01:14:37 +0000

    Bryan31

---
+++ bryan-zhang/poseidon/src/org/poseidon/action/demo/httpInvoke/TestMain.java
@@ -0,0 +1,13 @@
+package org.poseidon.action.demo.httpInvoke;
+
+import org.apache.commons.httpclient.NameValuePair;
+import org.poseidon.util.HttpClientUtil;
+
+public class TestMain {
+	public static void main(String[] args) throws Exception{
+    	NameValuePair[] data = {new NameValuePair("method", "returnXmlMethod")}; 
+
+    	String returnText = HttpClientUtil.sendPost("http://localhost:8085/poseidon/httpInvokeAction.do", data, 6000);
+    	System.out.println(returnText);
+    }
+}
