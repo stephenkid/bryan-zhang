e158721626fb9cfa6adb7340f134fe53411b48e7
bryan.zhang.31 <bryan.zhang.31@c3c2f732-976e-11de-9e98-d1f6c90d23ca>
2011-03-22 04:20:58 +0000

    bryan

--- poseidon/src/org/poseidon/component/dataExport/Test.java
+++ poseidon/src/org/poseidon/component/dataExport/Test.java
@@ -22,23 +22,82 @@ public class Test {
 		headMap.put("address", "地址");
 		headMap.put("birthday", "出生日期");
 		
-		List<Map<String, ?>> dataList = new ArrayList<Map<String,?>>();
-		Map<String, Object> dataMap = new HashMap<String, Object>();
-		dataMap.put("name", "jack");
-		dataMap.put("age", 31);
-		dataMap.put("address", "center Avu and abcefghijklmnopqrstuvwxyz");
-		dataMap.put("birthday", new Date());
-		dataList.add(dataMap);
-		dataMap = new HashMap<String, Object>();
-		dataMap.put("name", "rose");
-		dataMap.put("age", 25);
-		dataMap.put("address", "center Avu");
-		dataMap.put("birthday", new Date());
-		dataList.add(dataMap);
+//		List<Map<String, ?>> dataList = new ArrayList<Map<String,?>>();
+//		Map<String, Object> dataMap = new HashMap<String, Object>();
+//		dataMap.put("name", "jack");
+//		dataMap.put("age", 31);
+//		dataMap.put("address", "center Avu and abcefghijklmnopqrstuvwxyz");
+//		dataMap.put("birthday", new Date());
+//		dataList.add(dataMap);
+//		dataMap = new HashMap<String, Object>();
+//		dataMap.put("name", "rose");
+//		dataMap.put("age", 25);
+//		dataMap.put("address", "center Avu");
+//		dataMap.put("birthday", new Date());
+//		dataList.add(dataMap);
+		
+		List<Person> dataList = new ArrayList<Person>();
+		Person p = new Person();
+		p.setName("rose");
+		p.setAge(22L);
+		p.setAddress("asdfdasfsafasd");
+		p.setBirthday(new Date());
+		dataList.add(p);
+		p = new Person();
+		p.setName("mike bibi");
+		p.setAge(32L);
+		p.setAddress("asdfdasfsafasd");
+		p.setBirthday(new Date());
+		dataList.add(p);
 		
 		DataExport dataExport = new XlsExportImpl();
-		dataExport.convertList2File(dataList, headMap, "c:/tmp1.xls");
+		dataExport.convertList2File(dataList, headMap, "c:/tmp.xls");
 
+	}
+	
+	private static class Person{
+		private String name;
+		
+		private Long age;
+		
+		private String address;
+		
+		private Date birthday;
+
+		public Person() {
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public Long getAge() {
+			return age;
+		}
+
+		public void setAge(Long age) {
+			this.age = age;
+		}
+
+		public String getAddress() {
+			return address;
+		}
+
+		public void setAddress(String address) {
+			this.address = address;
+		}
+
+		public Date getBirthday() {
+			return birthday;
+		}
+
+		public void setBirthday(Date birthday) {
+			this.birthday = birthday;
+		}
 	}
 
 }
