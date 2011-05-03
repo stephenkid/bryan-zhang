3a9d3cbd0ba4dc3ed78c27f4b654c6392a18a664
bryan.zhang.31 <bryan.zhang.31@c3c2f732-976e-11de-9e98-d1f6c90d23ca>
2011-05-03 03:22:59 +0000

    bryan

---
+++ neptune/src/org/neptune/designPattern/defaultAdapter/AbstractServiceAdapter.java
@@ -0,0 +1,21 @@
+package org.neptune.designPattern.defaultAdapter;
+
+public abstract class AbstractServiceAdapter implements IService {
+
+	public void operateMethodA() {
+
+	}
+
+	public void operateMethodB() {
+
+	}
+
+	public void operateMethodC() {
+
+	}
+
+	public void operateMethodD() {
+
+	}
+
+}
---
+++ neptune/src/org/neptune/designPattern/defaultAdapter/IService.java
@@ -0,0 +1,11 @@
+package org.neptune.designPattern.defaultAdapter;
+
+public interface IService {
+	public void operateMethodA();
+	
+	public void operateMethodB();
+	
+	public void operateMethodC();
+	
+	public void operateMethodD();
+}
