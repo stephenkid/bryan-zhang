1249b81fafd7526bdb9f3e40b5713c82a1261796
bryan.zhang.31 <bryan.zhang.31@c3c2f732-976e-11de-9e98-d1f6c90d23ca>
2011-05-25 02:21:01 +0000

    更新

--- poseidon/src/org/poseidon/util/BeanUtil.java
+++ poseidon/src/org/poseidon/util/BeanUtil.java
@@ -6,20 +6,28 @@ import java.util.Map;
 
 public class BeanUtil {
 
+	/**
+	 * 从Map转换为bean对象。map键值要和bean一样。
+	 * @param map
+	 * @param t
+	 */
 	public static <T> void map2Bean(Map<String, Object> map, T t) {
 		Object[] objKeys = map.keySet().toArray();
 		String strFieldName = null;
+		Field objField = null;
 		try {
 			for (Object objkey : objKeys) {
 				strFieldName = objkey.toString();
-				Field objField = t.getClass().getDeclaredField(strFieldName);
+				objField = t.getClass().getDeclaredField(strFieldName);
 				objField.setAccessible(true);
 				objField.set(t, map.get(strFieldName));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+			t = null;
 		}
 	}
+
 
 	public static <T> Map<String, Object> bean2Map(T t) {
 		Map<String, Object> returnMap = new HashMap<String, Object>();
