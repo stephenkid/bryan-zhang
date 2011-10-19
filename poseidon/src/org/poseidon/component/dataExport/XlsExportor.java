e6671cb5d42be244cd125e99a946f0e6739a9b70
bryan.zhang.31 <bryan.zhang.31@c3c2f732-976e-11de-9e98-d1f6c90d23ca>
2011-10-19 10:20:15 +0000

    bryan

--- poseidon/src/org/poseidon/component/dataExport/XlsExportor.java
+++ poseidon/src/org/poseidon/component/dataExport/XlsExportor.java
@@ -175,6 +175,9 @@ public class XlsExportor{
 	
 	/***************************************************private class*****************************************************/
 	
+	/**
+	 * csv Fetch处理器
+	 */
 	private class CsvRowCallbackHandler implements RowCallbackHandler{
 	    
 	    private PrintWriter pw;
@@ -187,6 +190,7 @@ public class XlsExportor{
 	    
 	    public void processRow(ResultSet rs) throws SQLException {
 	        if (this.rowCnt == 0){
+	            rs.setFetchSize(1);
 	            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++){
 	                if (i == rs.getMetaData().getColumnCount() - 1){
 	                    this.writeToFile(pw, rs.getMetaData().getColumnName(i+1), true);
