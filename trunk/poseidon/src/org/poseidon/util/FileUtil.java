3236ef2ce1a763a18acd7e446116e9344cf7dee5
bryan.zhang.31 <bryan.zhang.31@c3c2f732-976e-11de-9e98-d1f6c90d23ca>
2011-10-21 02:15:54 +0000

    bryan

--- trunk/poseidon/src/org/poseidon/util/FileUtil.java
+++ trunk/poseidon/src/org/poseidon/util/FileUtil.java
@@ -6,11 +6,9 @@ import java.io.Closeable;
 import java.io.File;   
 import java.io.FileInputStream;   
 import java.io.FileReader;   
-import java.io.FileWriter;   
 import java.io.IOException;   
 import java.io.InputStream;   
 import java.io.InputStreamReader;   
-import java.nio.charset.Charset;   
 import java.util.ArrayList;   
 import java.util.List;   
 
