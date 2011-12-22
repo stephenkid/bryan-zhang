630febde18598ad64a8a2a9f1c6dc76eb41dc8a7
bryan.zhang.31 <bryan.zhang.31@c3c2f732-976e-11de-9e98-d1f6c90d23ca>
2011-12-22 06:27:25 +0000

    bryan

---
+++ trunk/aresServer/src/org/ares/dto/TestDto2.java
@@ -0,0 +1,18 @@
+package org.ares.dto;
+
+import java.io.Serializable;
+
+public class TestDto2 implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    private String t2;
+
+    public String getT2() {
+        return t2;
+    }
+
+    public void setT2(String t2) {
+        this.t2 = t2;
+    }
+}
