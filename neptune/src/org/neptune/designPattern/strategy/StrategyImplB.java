8c507924ac0d761534d8bab79f82f0350958b72f
bryan.zhang.31 <bryan.zhang.31@c3c2f732-976e-11de-9e98-d1f6c90d23ca>
2012-07-07 08:46:29 +0000

    bryan

---
+++ neptune/src/org/neptune/designPattern/strategy/StrategyImplB.java
@@ -0,0 +1,8 @@
+package org.neptune.designPattern.strategy;
+
+public class StrategyImplB implements IStrategy {
+
+	public void doSomething() throws Exception {
+		System.out.println("do strategy B");
+	}
+}
