3d8c391da4e683d934852d153a5bff4342a98c3e
bryan.zhang.31 <bryan.zhang.31@c3c2f732-976e-11de-9e98-d1f6c90d23ca>
2012-11-15 11:17:06 +0000

    bryan zhang

--- trunk/workFlowDemo2/src/com/myWorkFlow/base/Flow.java
+++ trunk/workFlowDemo2/src/com/myWorkFlow/base/Flow.java
@@ -1,9 +1,33 @@
 package com.myWorkFlow.base;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+import com.myWorkFlow.component.FlowComponent;
 
 public class Flow {
 	private List<FlowType> flowTypeList;
+	
+	//获取此FLOW里所有的顺序Component
+	public List<FlowComponent> getAllComponent(){
+		List<FlowComponent> cmpList = new ArrayList<FlowComponent>();
+		List<FlowComponent> cmpInnerList = null;
+		for (FlowType type : flowTypeList){
+			cmpInnerList = type.getComponentList();
+			//根据index排序
+			Collections.sort(cmpInnerList, new Comparator<FlowComponent>() {
+				public int compare(FlowComponent o1, FlowComponent o2) {
+					return (o1.getIndex() - o2.getIndex());
+				};
+			});
+			for (FlowComponent cmp : cmpInnerList){
+				cmpList.add(cmp);
+			}
+		}
+		return cmpList;
+	}
 	
 	public List<FlowType> getFlowTypeList() {
 		return flowTypeList;
