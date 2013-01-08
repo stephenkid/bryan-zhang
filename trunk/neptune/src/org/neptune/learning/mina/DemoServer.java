b97b537de4c9f8e25e2ab44018044720e5beb11b
bryan.zhang.31 <bryan.zhang.31@c3c2f732-976e-11de-9e98-d1f6c90d23ca>
2013-01-08 10:25:05 +0000

    bryan zhang

---
+++ trunk/neptune/src/org/neptune/learning/mina/DemoServer.java
@@ -0,0 +1,34 @@
+package org.neptune.learning.mina;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.LineDelimiter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+public class DemoServer {
+	private static int port = 3005;
+	
+	public static void main(String[] args){
+		IoAcceptor acceptor = null;
+		try{
+			acceptor = new NioSocketAcceptor();
+			acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(
+					Charset.forName("UTF-8"),
+					LineDelimiter.WINDOWS.getValue(),
+					LineDelimiter.WINDOWS.getValue())));
+			acceptor.getSessionConfig().setReadBufferSize(2048);
+			acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
+			acceptor.setHandler(new DemoHandler());
+			acceptor.bind(new InetSocketAddress(port));
+			System.out.println("server started,port:" + port);
+		}catch(Throwable t){
+			System.out.println("server started error");
+			t.printStackTrace();
+		}
+	}
+}
